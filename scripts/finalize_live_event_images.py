6693564f0ce7bdb384709b205ce0f8d7dbece881
84lorinw-a11y <84lorinw@gmail.com>
2026-09-02 06:12:35 -0500

    Use Marty solo image for Kuna show

--- scripts/finalize_live_event_images.py
+++ scripts/finalize_live_event_images.py
@@ -25,7 +25,7 @@ STALE_IMAGE_URLS = {
 # generic artwork. Real event artwork is always preserved.
 IMAGE_CANDIDATES = {
     "marty": [
-        "https://img1.wsimg.com/isteam/ip/6ed0aa91-488e-49ff-a53b-8d885654844e/DSC07306%20Edited.jpg/:/cr=t:0%25,l:0%25,w:100%25,h:100%25/rs=w:600,cg:true",
+        "https://i.scdn.co/image/ab6761610000e5eb3d2d9f74de93906d1f5996f3",
     ],
     "808 beezy": [
         "https://pbs.twimg.com/profile_images/1836827722309312512/e5kgorwv.jpg",
