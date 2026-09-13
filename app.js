017d893e6dd5e5d27ac6376e9a4598ad19a54b96
kingdom-circuit-catalog-audit <41898282+github-actions[bot]@users.noreply.github.com>
2026-09-13 14:24:45 +0000

    Apply verified Sep 11 catalog audit [catalog audit]

--- app.js
+++ app.js
@@ -1266,8 +1266,7 @@ const VERIFIED_ARTIST_REGISTRY_UPDATES = {
     "spotifyProfile": "https://open.spotify.com/artist/6j8t8rQzrAtRx5tYImodgd",
     "youtubeProfile": "https://www.youtube.com/channel/UCAvlfmD2aiqXxxknr-9VSVg",
     "officialImageSource": "https://www.instagram.com/cutthecho/",
-    "sourceRegistryVerified": true,
-    "sourceRegistryRosterOrder": 79
+    "sourceRegistryVerified": true
   },
   "dkg kie": {
     "aliases": [
@@ -1278,8 +1277,7 @@ const VERIFIED_ARTIST_REGISTRY_UPDATES = {
     "spotifyProfile": "https://open.spotify.com/artist/1eeYg6dFkaRT5GA0lsCVHA",
     "youtubeProfile": "https://www.youtube.com/@dkgkie",
     "officialImageSource": "https://www.instagram.com/dkg.kie",
-    "sourceRegistryVerified": true,
-    "sourceRegistryRosterOrder": 80
+    "sourceRegistryVerified": true
   },
   "braille": {
     "aliases": [
@@ -1290,8 +1288,7 @@ const VERIFIED_ARTIST_REGISTRY_UPDATES = {
     "spotifyProfile": "https://open.spotify.com/artist/6RYTz1tFNDF2qP0mwqEwDO",
     "youtubeProfile": "https://www.youtube.com/@bryanbraille",
     "officialImageSource": "https://www.humblebeast.com/music/braille",
-    "sourceRegistryVerified": true,
-    "sourceRegistryRosterOrder": 81
+    "sourceRegistryVerified": true
   },
   "canton jones": {
     "aliases": [
@@ -1302,8 +1299,7 @@ const VERIFIED_ARTIST_REGISTRY_UPDATES = {
     "spotifyProfile": "https://open.spotify.com/artist/3nzEXHMRFWTw4zt3pVRv6V",
     "youtubeProfile": "https://www.youtube.com/@CantonJones1",
     "officialImageSource": "https://www.instagram.com/thecantonjones/?hl=en",
-    "sourceRegistryVerified": true,
-    "sourceRegistryRosterOrder": 82
+    "sourceRegistryVerified": true
   },
   "jay-way": {
     "aliases": [
@@ -1315,8 +1311,7 @@ const VERIFIED_ARTIST_REGISTRY_UPDATES = {
     "spotifyProfile": "https://open.spotify.com/artist/1RDbE3dM2bNNSTh88R4MQ7",
     "youtubeProfile": "https://www.youtube.com/@JayWayTheAlien",
     "officialImageSource": "https://www.jaywaythealien.com/",
-    "sourceRegistryVerified": true,
-    "sourceRegistryRosterOrder": 83
+    "sourceRegistryVerified": true
   },
   "stixx aka conejo": {
     "aliases": [
@@ -1328,8 +1323,7 @@ const VERIFIED_ARTIST_REGISTRY_UPDATES = {
     "spotifyProfile": "https://open.spotify.com/artist/3khYLvZ6GmLlPMPlTfMTBr",
     "youtubeProfile": "https://www.youtube.com/@stixxwym/videos",
     "officialImageSource": "https://linktr.ee/stixxwym",
-    "sourceRegistryVerified": true,
-    "sourceRegistryRosterOrder": 84
+    "sourceRegistryVerified": true
   },
   "ruslan": {
     "aliases": [
@@ -1341,8 +1335,7 @@ const VERIFIED_ARTIST_REGISTRY_UPDATES = {
     "spotifyProfile": "https://open.spotify.com/artist/2GEXrCflKZ5S5ZHBM4LNcV",
     "youtubeProfile": "https://www.youtube.com/@RuslanKD/featured",
     "officialImageSource": "https://www.instagram.com/ruslankd/?hl=en",
-    "sourceRegistryVerified": true,
-    "sourceRegistryRosterOrder": 85
+    "sourceRegistryVerified": true
   },
   "j j l": {
     "aliases": [
@@ -2124,21 +2117,6 @@ const VERIFIED_ARTIST_REGISTRY_UPDATES = {
     "officialImageSource": "https://kadenjordan.com/",
     "imagePosition": "center",
     "sourceRegistryVerified": true
-  },
-  "caleb gordon": {
-    "aliases": [
-      "Caleb Gordon"
-    ],
-    "website": "https://tprlive.co/collections/caleb-gordon-the-eden-experience",
-    "instagramProfile": "https://www.instagram.com/calebfromeden/",
-    "spotifyProfile": "https://open.spotify.com/artist/6s3XaJkcT7464G4oII9V41",
-    "youtubeProfile": "https://www.youtube.com/@CalebGordon",
-    "officialImageSource": "https://tprlive.co/collections/caleb-gordon-the-eden-experience",
-    "imageUrl": "assets/artists/caleb-gordon-primary.jpg",
-    "imagePosition": "center",
-    "preferArtistImage": true,
-    "sourceRegistryVerified": true,
-    "sourceRegistryRosterOrder": 4
   }
 };
 const ARTIST_OVERRIDES = {
